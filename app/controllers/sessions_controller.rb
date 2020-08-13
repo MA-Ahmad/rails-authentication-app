6a745fad4253a3eed748259e7049b6240fe7559b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-12 21:22:59 -0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
+
     def create
-        user = User
-                .find_by(email: params["user"]["email"])
-                .try(:authenticate, params["user"]["password"])
+        user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
 
         if user
         session[:user_id] = user.id
@@ -14,5 +14,23 @@ class SessionsController < ApplicationController
         else
         render json: { status: 401 }
         end
+    end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def logout
+        reset_session
+        render json: { status: 200, logged_out: true }
     end
 end
