b57b8a5bf57fb06a2a665633817ae1c6f449365c
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-12 22:38:59 -0700

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "your-production-app-here.com"
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "your-production-app-here.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
