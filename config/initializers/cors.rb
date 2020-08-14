83dad4fe640a755ddb73251b260c904efea9ff0d
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-14 15:16:50 -0700

    updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://your-production-app-here.com"
+    origins "https://rails-auth-proj.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
