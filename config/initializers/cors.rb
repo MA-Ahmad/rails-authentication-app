c4c0fac1f55319b595c93ffc3371f4eaf18dcbc1
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-14 15:32:14 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://rails-auth-proj.herokuapp.com"
+    origins "https://rr-auth-app.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
